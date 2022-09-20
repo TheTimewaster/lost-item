45c7368b910bd2fdf133e856ade4eca0592d3f01
Tu <tht157@hotmail.de>
2022-09-20 23:11:25 +0200

    init commit

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtPage></NuxtPage>
+</template>
+<script lang="ts" setup>  
+useHead({
+  bodyAttrs: {
+    class: ['bg-pink-100'],
+  }
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="p-4 md:p-8">
+    <h1 class="text-5xl">Lost Item</h1>
+  </main>
+</template>
+
+<script setup lang="ts">
+  useHead({
+    title: 'Lost item | Your service for tagging your personal items'
+  })
+</script>
+
+<style scoped>
+
+</style>
