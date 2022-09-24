ee0cdca03d2d439f384e72dba0a0100ae7ba0deb
Tu <tht157@hotmail.de>
2022-09-24 16:13:18 +0200

    changed background color of all layouts

--- layouts/application.vue
+++ layouts/application.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-useHead({
-  bodyAttrs: {
-    class: ['bg-rose-50'],
-  },
-});
 </script>
 
 <template>
   <div class="pt-14">
-    <app-bar class="fixed top-0 z-10" />
+    <app-bar class="fixed top-0 z-10 border-b-light-900 border-b-1" />
     <nuxt-page class="max-w-7xl mx-auto p-4" />
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,4 @@
 <script lang="ts" setup>
-useHead({
-  bodyAttrs: {
-    class: ['bg-rose-50'],
-  },
-});
 </script>
 
 <template>
