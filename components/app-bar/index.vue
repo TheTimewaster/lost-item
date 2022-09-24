8e02ec14489357fa7cc054661d64393cf7e6fad8
Tu <tht157@hotmail.de>
2022-09-24 02:36:39 +0200

    introduced application layout

---
+++ components/app-bar/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex justify-between w-full px-4 py-2 bg-white">
+    <span class="text-4xl font-semibold font-serif"> Quý </span>
+    <button class="py-2 px-4 bg-yellow-400 rounded-lg" @click="logout">
+      Logout
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { $appwriteAccount } = useNuxtApp();
+const router = useRouter();
+const logout = async () => {
+  await $appwriteAccount.deleteSessions();
+  router.push('/');
+};
+</script>
+
+<style scoped></style>
---
+++ layouts/application.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pt-14">
+    <app-bar class="fixed top-0 z-10" />
+    <nuxt-page class="max-w-7xl mx-auto p-4"></nuxt-page>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: ['bg-rose-50'],
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
