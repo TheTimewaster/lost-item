834f59d84bb9d56e02c586b908a87ed7cef1069f
Tu <tht157@hotmail.de>
2022-09-24 16:12:03 +0200

    add link to home page for app bar

--- components/app-bar/index.vue
+++ components/app-bar/index.vue
@@ -9,8 +9,10 @@ const logout = async () => {
 
 <template>
   <div class="flex justify-between w-full px-4 py-2 bg-white">
-    <span class="text-4xl font-semibold font-serif"> Quý </span>
-    <button class="py-2 px-4 bg-yellow-400 rounded-lg" @click="logout">
+    <nuxt-link to="/">
+      <span class="text-4xl font-semibold font-serif"> Quý </span>
+    </nuxt-link>
+    <button class="py-2 px-4 bg-lemon-500 rounded-lg font-bold" @click="logout">
       Logout
     </button>
   </div>
