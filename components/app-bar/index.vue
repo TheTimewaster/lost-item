ead01df28ee04b02df2922a82b23f463a6e224a2
Tu <tht157@hotmail.de>
2022-09-25 03:12:55 +0200

    sort windi classes on save

--- components/app-bar/index.vue
+++ components/app-bar/index.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 const { $appwriteAccount } = useNuxtApp();
 const router = useRouter();
+const sessionCookie = useCookie('quy-session');
 const logout = async () => {
+  sessionCookie.value = '';
   await $appwriteAccount.deleteSessions();
+
   router.push('/');
 };
 </script>
 
 <template>
-  <div class="flex justify-between w-full px-4 py-2 bg-white">
+  <div class="bg-white flex w-full py-2 px-4 justify-between">
     <nuxt-link to="/">
-      <span class="text-4xl font-font-bold font-serif"> Quý </span>
+      <span class="font-font-bold font-serif text-4xl"> Quý </span>
     </nuxt-link>
-    <button class="py-2 px-4 bg-lemon-500 rounded-lg font-bold" @click="logout">
+    <button class="rounded-lg font-bold bg-lemon-500 py-2 px-4" @click="logout">
       Logout
     </button>
   </div>
