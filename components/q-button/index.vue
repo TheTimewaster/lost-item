c4a182a9cf594d9418fe333f3081dacea4df34b1
Tu <tht157@hotmail.de>
2023-04-10 22:18:27 +0200

    buttons overhaul to oruga button

--- components/q-button/index.vue
+++ components/q-button/index.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+import OButton from '@oruga-ui/oruga-next/src/components/button/Button.vue';
+import { Icon } from '@iconify/vue';
 import useClasses from './useClasses';
 
 const props = withDefaults(defineProps<{
+  component?: 'button' | 'nuxt-link' | 'a'
+  to?: string | object
   type?: 'primary' | 'secondary'
   label?: string | number
+  icon?: string
   square?: boolean
   round?: boolean
   size?: 'sm' | 'lg'
 }>(), {
+  to: '',
   type: 'primary',
   square: false,
   round: false,
@@ -15,7 +21,9 @@ const props = withDefaults(defineProps<{
 });
 
 const emit = defineEmits<{ (e: 'click', event: Event): void }>();
-const handleClick = (event: Event) => emit('click', event);
+function handleClick(event: Event) {
+  emit('click', event);
+}
 const classes = useClasses(props);
 
 defineExpose({
@@ -25,13 +33,56 @@ defineExpose({
 </script>
 
 <template>
-  <button
-    class="rounded-xl font-bold"
-    :class="classes"
+  <OButton
+    v-if="component === 'nuxt-link'"
+    tag="router-link"
+    :root-class="classes"
+    :to="to"
+    override
+  >
+    <slot>
+      <Icon
+        v-if="icon != null"
+        :icon="icon"
+        :name="icon"
+        :class="{ 'mr-2': label != null }"
+      />
+      {{ label }}
+    </slot>
+  </OButton>
+
+  <OButton
+    v-else-if="component === 'a'"
+    tag="a"
+    :root-class="classes"
+    :to="to"
+    override
+  >
+    <slot>
+      <Icon
+        v-if="icon != null"
+        :icon="icon"
+        :name="icon"
+        :class="{ 'mr-2': label != null }"
+      />
+      {{ label }}
+    </slot>
+  </OButton>
+
+  <OButton
+    v-else
+    :root-class="classes"
+    override
     @click="handleClick"
   >
     <slot>
+      <Icon
+        v-if="icon != null"
+        :icon="icon"
+        :name="icon"
+        :class="{ 'mr-2': label != null }"
+      />
       {{ label }}
     </slot>
-  </button>
+  </OButton>
 </template>
--- components/q-button/useClasses.ts
+++ components/q-button/useClasses.ts
@@ -1,14 +1,18 @@
 export default (props: { type: 'primary' | 'secondary'; size: 'lg' | 'sm'; square: boolean; round: boolean }) => {
-  const classes = computed(() => ({
-    'rounded-xl font-bold': true,
-    'bg-lemon-500 hover:bg-lemon-400 text-black': props.type === 'primary',
-    'bg-gray-200 hover:bg-gray-300 dark:bg-dark-200 dark:hover:bg-dark-100 dark:text-white text-black': props.type === 'secondary',
-    'w-12': props.square && props.size === 'lg',
-    'h-12 px-4 py-2': props.size === 'lg',
-    'w-8': props.square && props.size === 'sm',
-    'h-8 px-2 py-1 leading-none rounded-lg': props.size === 'sm',
-    '!rounded-full': props.round,
-  }));
+  const classes = computed(() => {
+    const obj = {
+      'font-bold leading-none': true,
+      'bg-lemon-500 hover:bg-lemon-400 !text-black': props.type === 'primary',
+      'bg-gray-200 hover:bg-gray-300 text-black dark:(bg-dark-200 hover:bg-dark-100 text-white)': props.type === 'secondary',
+      'w-12': props.square && props.size === 'lg',
+      'h-12 px-4 py-4 rounded-xl': props.size === 'lg',
+      'w-8': props.square && props.size === 'sm',
+      'h-8 px-2 py-1 rounded-lg': props.size === 'sm',
+      '!rounded-full': props.round,
+    };
+
+    return Object.entries(obj).filter(([_, value]) => value).map(([key]) => key).join(' ');
+  });
 
   return classes;
 };
