119f4dc9c98bf42fb46fb6314218a2706b8a7b13
Tu <tht157@hotmail.de>
2023-04-10 22:18:41 +0200

    migrate q-status to oruga tooltip

--- components/q-status.vue
+++ components/q-status.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Icon } from '@iconify/vue';
+import OTooltip from '@oruga-ui/oruga-next/src/components/tooltip/Tooltip.vue';
 import { ItemStatus } from '~~/types/models';
 
 const props = defineProps<{
@@ -11,51 +13,64 @@ defineExpose({
 
 const i18n = useI18n();
 
-const model = computed<{ label: string; icon: string; tooltip: string; color: string }>(() => {
+const model = computed<{ label: string; icon: string; tooltip: string; itemClass: string }>(() => {
   switch (props.status) {
     case ItemStatus.ACTIVE:
       return {
         label: i18n.t('app.component.status.active.label'),
         icon: 'fa6-solid:check',
         tooltip: i18n.t('app.component.status.active.tooltip'),
-        color: 'bg-green',
+        itemClass: 'bg-green text-white',
       };
     case ItemStatus.FOUND:
       return {
         label: i18n.t('app.component.status.found.label'),
         icon: 'fa6-solid:hands-holding',
         tooltip: i18n.t('app.component.status.found.tooltip'),
-        color: 'bg-sky',
+        itemClass: 'bg-sky text-white',
       };
     case ItemStatus.LOST:
       return {
         label: i18n.t('app.component.status.lost.label'),
         icon: 'fa6-solid:magnifying-glass',
         tooltip: i18n.t('app.component.status.lost.tooltip'),
-        color: 'bg-red',
+        itemClass: 'bg-red text-white',
       };
     case ItemStatus.LOST_FOREVER:
       return {
         label: i18n.t('app.component.status.lost_forever.label'),
         icon: 'fa6-solid:xmark',
         tooltip: i18n.t('app.component.status.lost_forever.tooltip'),
-        color: 'bg-bg-gray-100',
+        itemClass: 'bg-gray-200 text-black',
       };
   }
 });
 </script>
 
 <template>
-  <span
-    v-tooltip="model.tooltip"
-    class="rounded-md font-bold text-white text-sm py-1 px-2 capitalize"
-    :class="{
-      [model.color]: true,
-    }"
+  <OTooltip
+    :label="model.tooltip"
+    size="large"
+    position="auto"
+    multiline
+    content-class="!bg-white text-black rounded-lg shadow-xl min-w-80 px-4 py-2 !text-left dark:(!bg-gray-900 text-white)"
+    arrow-class="hidden"
   >
-    <Icon :name="model.icon" class="mr-1" />
-    {{ model.label }}
-  </span>
+    <span
+      class="rounded-md flex font-bold text-sm py-1 px-2 items-center capitalize"
+      :class="{
+        [model.itemClass]: true,
+
+      }"
+    >
+      <Icon
+        :icon="model.icon"
+        :name="model.icon"
+        class="mr-2"
+      />
+      {{ model.label }}
+    </span>
+  </OTooltip>
 </template>
 
 <style lang="postcss">
