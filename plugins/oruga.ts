35a00d5cf4cdcd3100518f09cca7d91244c70031
Tu <tht157@hotmail.de>
2023-04-10 22:20:50 +0200

    install oruga

---
+++ plugins/oruga.ts
@@ -0,0 +1,5 @@
+import '@oruga-ui/oruga-next/dist/oruga.css';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  // nuxtApp.vueApp.use(Oruga);
+});
--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,16 +1,10 @@
 import PrimeVue from 'primevue/config';
-import Menu from 'primevue/menu/Menu.vue';
 import ToastService from 'primevue/toastservice';
-import Toast from 'primevue/toast/Toast.vue';
-import Tooltip from 'primevue/tooltip';
 import { defineNuxtPlugin } from '#app';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, { ripple: false });
   nuxtApp.vueApp.use(ToastService);
-  nuxtApp.vueApp.component('Menu', Menu);
-  nuxtApp.vueApp.component('Toast', Toast);
-  nuxtApp.vueApp.directive('tooltip', Tooltip);
 
   // other components that you need
 });
